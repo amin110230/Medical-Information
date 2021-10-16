f2b2c0758cd78820731d1999d082853ab44b7724
Sadman Sobhan <imran110219@gmail.com>
2021-10-16 21:52:18 +0600

    Second commit

--- medical-information-angular/src/app/app-routing.module.ts
+++ medical-information-angular/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DoctorComponent } from './components/doctor/doctor.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
-const routes: Routes = [{ path: 'doctors', component: DoctorComponent }];
+const routes: Routes = [
+  { path: 'home', component: DoctorComponent },
+  { path: 'doctor', component: DoctorComponent },
+  { path: 'hospital', component: DoctorComponent },
+  { path: 'profile', component: DoctorComponent },
+  { path: 'signup', component: DoctorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- medical-information-angular/src/app/app.module.ts
+++ medical-information-angular/src/app/app.module.ts
@@ -1,38 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTableModule } from '@angular/material/table';
-import { MatSelectModule } from '@angular/material/select';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
-import { DoctorComponent } from './components/doctor/doctor.component';
-import {FormControl} from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, DoctorComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatTableModule,
-    MatSelectModule,
-    FormsModule,
-    FormControl,
-    FlexLayoutModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
