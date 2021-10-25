b511663d45b8052642968ed806f7243aaf13f322
Shahjahan <shahjahan.dohatec@gmail.com>
2021-10-25 17:59:54 +0600

    Responsive navbar added

--- medical-information-angular/src/app/app.module.ts
+++ medical-information-angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { DoctorComponent } from './doctor/doctor.component';
@@ -18,9 +20,11 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    FlexLayoutModule,
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
+    MatListModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     AppRoutingModule,
